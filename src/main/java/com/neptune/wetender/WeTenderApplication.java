c0a5efb3a940f200f41f3d0c7e9271dbdeb19274
Neptunr164 <179183669+Neptune164@users.noreply.github.com>
2026-06-29 14:26:19 +0800

    add mongodb database and uri

---
+++ src/main/java/com/neptune/wetender/WeTenderApplication.java
@@ -0,0 +1,13 @@
+package com.neptune.wetender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeTenderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeTenderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/neptune/wetender/WeTenderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neptune.wetender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeTenderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
